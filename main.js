ae8afb847103bcbeff1910bda7b9564e4f8e3959
Sohail Shaikh <sohailhacker7786@gmail.com>
2024-01-02 21:39:21 +0530

    02/01/2024

--- main.js
+++ main.js
@@ -4,3 +4,26 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+
+var imageCon = document.querySelector("#elem-container");
+var fixedImg = document.querySelector("#fixed-image");
+
+imageCon.addEventListener("mouseenter", function () {
+    fixedImg.style.display = "block";
+    fixedImg.style.objectFit = "cover";
+});
+
+
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function(e){
+    e.addEventListener("mouseenter", function(){
+        var img = e.getAttribute("data-img")
+        fixedImg.style.backgroundImage = `url(${img})`
+    })
+})
+
+
+imageCon.addEventListener("mouseleave", function () {
+    fixedImg.style.display = "none";
+});
